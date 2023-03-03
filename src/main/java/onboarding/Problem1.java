6547f48553b49ecdd9dc2673ccbda89b8a6f808f
onegqueen <rkdwldnjs30@naver.com>
2023-03-03 21:25:30 +0900

    sum() : 매개변수에 대해 각 자리수를 더한값과 각 자리수를 곱한 값중 큰 값을 리턴

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,6 +4,17 @@ import java.util.List;
 
 class Problem1 {
 
+    public static int sum(int x){
+        int result_sum = 0;
+        int result_mul = 1;
+        while (x!=0){
+            result_sum+=(x%10);
+            result_mul*=(x%10);
+            x/=10;
+        }
+        return Math.max(result_sum,result_mul);
+    }
+
     public static class PageOrderException extends Exception{
         public PageOrderException(){ }
         public PageOrderException(String message){
@@ -20,6 +31,7 @@ class Problem1 {
             if((left>=1 && left<=399)|| left%2==1 || left+1!=right) {
                 throw new PageOrderException("예외사항 발생");
             }
+            max_num = Math.max(sum(left),sum(right));
         }
 
     }
