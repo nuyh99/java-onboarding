26e19672dac9228acb3412d754353658da9daea1
onegqueen <rkdwldnjs30@naver.com>
2023-03-05 18:26:33 +0900

    입력받은 문자열을 한문자씩 Frog 메소드를 통해 문자 변환후 빈문자열에 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -18,6 +18,10 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
 
+        for(int i = 0;i<word.length();i++){
+            answer+=Frog(word.charAt(i));
+        }
+
         return answer;
     }
 }
