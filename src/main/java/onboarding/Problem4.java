42e055d60c8cab14d03820536617ad31ddf355ee
onegqueen <rkdwldnjs30@naver.com>
2023-03-05 18:25:46 +0900

    Frog():문자를 사전대로 바꾸어주는 메소드 작성

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.Scanner;
 public class Problem4 {
+    public static final int CAPITAL = (int)'A'+(int)'Z';
+    public static final int SMALL = (int)'a'+(int)'z';
+    public static char Frog (char c) {
+        if ((int)c>=(int)'A' && (int)c<=(int)'Z'){
+            return (char) (CAPITAL - (int) c);
+        }
+        else if((int)c>=(int)'a' && (int)c<=(int)'z'){
+            return (char) (SMALL - (int) c);
+        }
+        else{
+            return c;
+        }
+    }
     public static String solution(String word) {
         String answer = "";
+
         return answer;
     }
 }
