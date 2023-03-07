117c8a4da0561fc39927e8868d9a4410aac9c1ad
onegqueen <rkdwldnjs30@naver.com>
2023-03-07 16:11:21 +0900

    CLAP(): 하나의 숫자에 대해 박수를 쳐야할 횟수를 리턴

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,23 @@
 package onboarding;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.mockito.internal.util.collections.ListUtil.filter;
+
 public class Problem3 {
+    public static final String Clap_num = "369";
+    public static long Clap (String str) {
+        int clap = 0;
+        while(str.length()!=0) {
+            String s = str.substring(str.length() - 1, str.length());
+            str = str.substring(0, str.length() - 1);
+            if (Clap_num.contains(s)) {
+                clap += 1;
+            }
+        }
+        return clap;
+    }
     public static int solution(int number) {
         int answer = 0;
         return answer;
