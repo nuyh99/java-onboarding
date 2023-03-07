6d25fbbef6d24387d1b37ec626f7f520b311743b
onegqueen <rkdwldnjs30@naver.com>
2023-03-07 15:30:09 +0900

    compare(str1,str2) : 두 문자열을 비교하여 겹치는 부분이 있는지 확인

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,39 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Problem6 {
+
+    public static int compare (String str1, String str2) {
+        for (int i = 0;i<str1.length()-1;i++){
+            String s1 = str1.substring(i,i+2);
+
+            if (str2.contains(s1)) {
+                return 0;
+            }
+        }
+        return -1;
+    }
+
+    public static  ArrayList<Integer> init(ArrayList<Integer> l,int size){
+        for(int i = 0;i<size;i++){
+            l.add(0);
+        }
+        return l;
+    }
+
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        ArrayList<String> answer = new ArrayList<String>();
+        ArrayList<Integer> check = new ArrayList<Integer>();
+        check = init(check,forms.size());
+
+
         return answer;
+
     }
 }
