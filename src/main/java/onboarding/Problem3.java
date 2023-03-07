e04f79933ede342bccaad70841cfc954ac7c32e6
onegqueen <rkdwldnjs30@naver.com>
2023-03-07 16:11:55 +0900

    1부터 number까지의 숫자에 대해 CLAP을 구하여 answer에 더함

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -20,6 +20,10 @@ public class Problem3 {
     }
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 1;i<=number;i++){
+            String str1 = Integer.toString(i);
+            answer+=Clap(str1);
+        }
         return answer;
     }
 }
